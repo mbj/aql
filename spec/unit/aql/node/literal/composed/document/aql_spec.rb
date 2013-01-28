00adde82cf8a35e818ac7d57d70a4afa6da3cce9
Markus Schirp <mbj@seonic.net>
2013-01-28 11:30:57 +0100

    Exclude test for 1.9 ordered hash properties

--- spec/unit/aql/node/literal/composed/document/aql_spec.rb
+++ spec/unit/aql/node/literal/composed/document/aql_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe AQL::Node::Literal::Composed::Document, '#aql' do
   let(:object) { AQL::Node::Literal.build(input) }
 
+  if RUBY_VERSION < '1.9'
+    pending 'Test for ordered hashes, ruby 1.8 has unordered ones'
+  end
+
   examples = {
     {}                    => '{}',
     { 'name' => 'Peter' } => '{"name": "Peter"}',

4e6740042f2af9e164c81ceaa20cb6111db6c750
Markus Schirp <mbj@seonic.net>
2013-02-11 20:33:27 +0100

    Cleanup implementation of FOR statement emitter

--- lib/aql/node/operation/for.rb
+++ lib/aql/node/operation/for.rb
@@ -3,7 +3,7 @@ module AQL
     class Operation
       # For operation
       class For < self
-        include Composition.new(:left, :right, :body)
+        include Composition.new(:local, :source, :body)
 
       private
 
@@ -17,37 +17,57 @@ module AQL
         #
         def emit(buffer)
           buffer.append('FOR ')
-          left.visit(buffer)
-          buffer.append(' IN ')
-          emit_right(buffer)
-          buffer.append(' ')
+          emit_local(buffer)
+          emit_source(buffer)
+          emit_body(buffer)
+        end
+
+        # Emit body
+        #
+        # @return [undefined]
+        #
+        # @api private
+        #
+        def emit_body(buffer)
           body.visit(buffer)
-          buffer.append('')
         end
 
-        # Emit right
+        # Emit local
         #
         # @return [undefined]
         #
         # @api private
         #
-        def emit_right(buffer)
-          if right.kind_of?(Name)
-            right.visit(buffer)
+        def emit_local(buffer)
+          local.visit(buffer)
+          buffer.append(' IN ')
+        end
+
+        # Emit source
+        #
+        # @return [undefined]
+        #
+        # @api private
+        #
+        def emit_source(buffer)
+          local = source
+          if local.kind_of?(Name)
+            local.visit(buffer)
           else
-            emit_right_parantheses(buffer)
+            emit_source_parentheses(buffer)
           end
+          buffer.append(' ')
         end
 
-        # Emit right in parantheses
+        # Emit source in parentheses
         #
         # @return [undefined]
         #
         # @api private
         #
-        def emit_right_parantheses(buffer)
-          buffer.parantheses do
-            right.visit(buffer)
+        def emit_source_parentheses(buffer)
+          buffer.parentheses do
+            source.visit(buffer)
           end
         end
 
